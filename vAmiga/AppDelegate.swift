a59f5bb98870fed40c2803916e70e5c4798e5104
Dirk Hoffmann <dirk.hoffmann@me.com>
2019-01-12 14:14:15 +0100

    Initial Commit

---
+++ vAmiga/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  vAmiga
+//
+//  Created by Dirk Hoffmann on 12.01.19.
+//  Copyright © 2019 Dirk Hoffmann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ vAmiga/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  vAmiga
+//
+//  Created by Dirk Hoffmann on 12.01.19.
+//  Copyright © 2019 Dirk Hoffmann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
