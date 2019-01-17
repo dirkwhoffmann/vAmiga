27389a5c446ded5c090f0402531de0b2aa671135
Dirk Hoffmann <dirk.hoffmann@me.com>
2019-01-17 15:49:46 +0100

    Turned the G64 media dialog into an ADF media dialog.

--- GUI/Dialogs/MediaBrowsers/DiskMountController.swift
+++ GUI/Dialogs/MediaBrowsers/DiskMountController.swift
@@ -1,11 +1,11 @@
-//
-// This file is part of VirtualC64 - A cycle accurate Commodore 64 emulator
+// -----------------------------------------------------------------------------
+// This file is part of vAmiga
 //
 // Copyright (C) Dirk W. Hoffmann. www.dirkwhoffmann.de
 // Licensed under the GNU General Public License v3
 //
 // See https://www.gnu.org for license information
-//
+// -----------------------------------------------------------------------------
 
 import Foundation
 
@@ -14,20 +14,65 @@ class DiskMountController : UserDialogController {
     var disk: AnyDiskProxy!
     
     let bytesPerRow = 32
-    var halftrack = 35    // Selected halftrack to display
-    var sizeInBytes = 0   // Number of bytes in the selected halftrack
-    var sizeInBits = 0    // Number of bits in the selected halftrack
+    
+    // Cylinder, Head, Track, and Sector
+    var _cylinder = 0
+    var _head = 0
+    var _track = 0
+    var _sector = 0
+        
+    func setCylinder(_ newValue: Int) {
+
+        if (newValue >= 0 && newValue <= 79) {
+            
+            _cylinder = newValue
+            _track    = _cylinder * 2 + _head
+            _sector   = (_track * 11) + (_sector % 11)
+        }
+    }
+    
+    func setHead(_ newValue: Int) {
+        
+        if (newValue >= 0 && newValue <= 1) {
+            
+            _head     = newValue
+            _track    = _cylinder * 2 + _head
+            _sector   = (_track * 11) + (_sector % 11)
+        }
+    }
+    
+    func setTrack(_ newValue: Int) {
+        
+        if (newValue >= 0 && newValue <= 159) {
+            
+            _track    = newValue
+            _cylinder = _track / 2
+            _head     = _track % 2
+            _sector   = (_track * 11) + (_sector % 11)
+        }
+    }
+
+    func setSector(_ newValue: Int) {
+        
+        if (newValue >= 0 && newValue <= 1759) {
+            
+            _sector   = newValue
+            _track    = _sector % 11
+            _cylinder = _track / 2
+            _head     = _track % 2
+        }
+    }
     
     // Outlets
-    @IBOutlet weak var icon: NSImageView!
-    @IBOutlet weak var header: NSTextField!
-    @IBOutlet weak var subheader: NSTextField!
-    @IBOutlet weak var subsubheader: NSTextField!
-    @IBOutlet weak var contents: NSTableView!
-    @IBOutlet weak var trackinfo: NSTextField!
-    @IBOutlet weak var trackSizeinfo: NSTextField!
-    @IBOutlet weak var stepper: NSStepper!
-    @IBOutlet weak var driveSelector: NSPopUpButton!
+    @IBOutlet weak var previewTable: NSTableView!
+    @IBOutlet weak var cylinderField: NSTextField!
+    @IBOutlet weak var cylinderStepper: NSStepper!
+    @IBOutlet weak var headField: NSTextField!
+    @IBOutlet weak var headStepper: NSStepper!
+    @IBOutlet weak var trackField: NSTextField!
+    @IBOutlet weak var trackStepper: NSStepper!
+    @IBOutlet weak var sectorField: NSTextField!
+    @IBOutlet weak var sectorStepper: NSStepper!
     
     override func showSheet(completionHandler handler:(() -> Void)? = nil) {
     
@@ -39,68 +84,80 @@ class DiskMountController : UserDialogController {
     }
     
     override public func awakeFromNib() {
- 
-        let numItems = disk.numberOfHalftracks()
-  
-        stepper.minValue = 1
-        stepper.maxValue = Double(numItems)
-        stepper.integerValue = halftrack
-        
-        // Configure directory window
-        // directory.intercellSpacing = NSSize(width: 0, height: 0)
-        
-        // Set icon and title
-        switch disk.type() {
-            
-        case G64_FILE:
-            icon.image = NSImage.init(named: "IconD64")
-            header.stringValue = "G64 Format"
-            break;
 
-        default:
-            assert(false)
-            break;
-        }
-        
         update()
     }
     
     func update() {
     
-        let t = (halftrack / 2) + 1
-        
-        disk.selectHalftrack(halftrack)
-        sizeInBytes = disk.sizeOfHalftrack()
-        sizeInBits = sizeInBytes * 8
-        trackSizeinfo.stringValue = String(format: "%d Bytes", sizeInBytes)
-        
-        if (halftrack % 2 == 0) {
-            let info = String(format: "Contents of halftrack %d:", halftrack)
-            trackinfo.stringValue = String(info)
-        } else {
-            let info = String(format: "Contents of halftrack %d (track %d):", halftrack, t)
-            trackinfo.stringValue = String(info)
-        }
-        
-        contents.reloadData()
+        cylinderField.integerValue   = _cylinder
+        cylinderStepper.integerValue = _cylinder
+        headField.integerValue       = _head
+        headStepper.integerValue     = _head
+        trackField.integerValue      = _track
+        trackStepper.integerValue    = _track
+        sectorField.integerValue     = _sector
+        sectorStepper.integerValue   = _sector
+
+        previewTable.reloadData()
     }
     
+    //
     // Action methods
-    
-    @IBAction func stepperAction(_ sender: Any!) {
+    //
+
+    @IBAction func cylinderAction(_ sender: NSTextField!) {
         
-        let sender = sender as! NSStepper
-        halftrack = sender.integerValue
+        setCylinder(sender.integerValue)
         update()
     }
-    
-    @IBAction override func okAction(_ sender: Any!) {
+    @IBAction func cylinderStepperAction(_ sender: NSStepper!) {
         
-        let nr = (driveSelector.selectedItem!.tag == 1) ? 1 : 2
+        setCylinder(sender.integerValue)
+        update()
+    }
+    @IBAction func headAction(_ sender: NSTextField!) {
         
-        myController?.changeDisk(disk, drive: nr)
+        setHead(sender.integerValue)
+        update()
+    }
+    @IBAction func headStepperAction(_ sender: NSStepper!) {
+        
+        setHead(sender.integerValue)
+        update()
+    }
+    @IBAction func trackAction(_ sender: NSTextField!) {
+        
+        setTrack(sender.integerValue)
+        update()
+    }
+    @IBAction func trackStepperAction(_ sender: NSStepper!) {
+        
+        setTrack(sender.integerValue)
+        update()
+    }
+    @IBAction func sectorAction(_ sender: NSTextField!) {
+        
+        setSector(sender.integerValue)
+        update()
+    }
+    @IBAction func sectorStepperAction(_ sender: NSStepper!) {
+        
+        setSector(sender.integerValue)
+        update()
+    }
+    @IBAction func df0Action(_ sender: Any!) {
+        
+        print("df0Action")
+        // myController?.changeDisk(disk, drive: nr)
         myController?.metalScreen.rotateBack()
+        hideSheet()
+    }
+    @IBAction func df1Action(_ sender: Any!) {
         
+        print("df1Action")
+        // myController?.changeDisk(disk, drive: nr)
+        myController?.metalScreen.rotateBack()
         hideSheet()
     }
 }
@@ -125,23 +182,15 @@ extension DiskMountController : NSTableViewDataSource {
     
     func numberOfRows(in tableView: NSTableView) -> Int {
         
-        return sizeInBytes / bytesPerRow
+        return 8
     }
     
     func tableView(_ tableView: NSTableView,
                    objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
         
-        if (tableColumn?.identifier)!.rawValue == "addr" {
-            
-            let s = String(format:"$%04X", row * bytesPerRow)
-            return s
-        }
-        
         if (tableColumn?.identifier)!.rawValue == "data" {
             
-            disk.selectHalftrack(halftrack)
-            disk.seekHalftrack(row * bytesPerRow)
-            return disk.readHalftrackHex(bytesPerRow)
+            return "42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42 42"
         }
         
         return "???"
