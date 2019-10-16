523dc0f6e7255a1c7e729795d1525213912a4d48
Dirk Hoffmann <dirk.hoffmann@me.com>
2019-10-16 17:38:04 +0200

    v0.42.1

--- Amiga/Foundation/va_config.h
+++ Amiga/Foundation/va_config.h
@@ -80,7 +80,7 @@ static const int BLT_CHECKSUM  = 2;  // Blitter checksums
 // #define BORDER_DEBUG     // Draws the border in debug colors
 // #define PIXEL_DEBUG      // Highlight first pixel in each 16-bit pixel chunk
 // #define LINE_DEBUG       // Colorizes certain rasterlines
-#define ALIGN_DRIVE_HEAD // Makes drive operations deterministic
+// #define ALIGN_DRIVE_HEAD // Makes drive operations deterministic
 // #define SLOW_BLT_DEBUG   // Execute all slow Blitter instructions in one chunk
 #define AGNUS_EXEC_DEBUG // Falls back to a simpler Agnus execution function
 
