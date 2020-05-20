dirkwhoffmann/vAmiga

commit cecd3f0d3f076fa3ffd8817920041e9f4b191d0e

Passing Blitter/irqtim1

Emulator/Agnus/Blitter.cpp
@@ -873,17 +873,14 @@ void
 Blitter::endBlit()
 {
     plaindebug(BLTTIM_DEBUG, "(%d,%d) Blitter terminates\n", agnus.pos.v, agnus.pos.h);
-
+    
     running = false;
-
-    // Trigger the Blitter interrupt
-    paula.raiseIrq(INT_BLIT);
-
+    
     // Clear the Blitter slot
     agnus.cancel<BLT_SLOT>();
 
     // Dump checksums if requested
-    if (BLT_CHECKSUM) { // && (bltsizeH != 1 || bltsizeV != 4)
+    if (BLT_CHECKSUM) {
         plaindebug("BLITTER check1: %x check2: %x ABCD: %x %x %x %x\n",
                    check1, check2, bltapt, bltbpt, bltcpt, bltdpt);
         plaindebug("Memory: %x (%x)\n",

Emulator/Agnus/SlowBlitter.cpp
@@ -152,8 +152,7 @@ Blitter::initSlowBlitter()
             }
         },
 
-        // 1: D0 -- D1 -- | -- D2 (most likely wrong, see correction below)
-        // 1: D0 -- -- D1 -- -- | -- D2
+        // 1:  -- D0 -- D1 | -- D2
         {
             {
                 {   // Full execution, no fill
@@ -970,6 +969,9 @@ Blitter::exec()
         busidle = instr & BUSIDLE;
     }
 
+    // Trigger Blitter interrupt if this is the termination cycle
+    if (instr & BLTDONE) paula.raiseIrq(INT_BLIT);
+    
     // Allocate the bus if needed
     if (bus && !agnus.allocateBus<BUS_BLITTER>()) return;
 

commit 1f5d4d87f80c77a2f7461c476155b9162da53641

Fixed blitter interrupt bug introduced in cecd3f0d

Emulator/Agnus/Blitter.cpp
@@ -817,6 +817,7 @@ Blitter::prepareBlit()
     running = true;
     bzero = true;
     bbusy = true;
+    birq = false;
 
     bltpc = 0;
     iteration = 0;

Emulator/Agnus/Blitter.h
@@ -145,6 +145,9 @@ class Blitter : public AmigaComponent
     // The Blitter zero flag
     bool bzero;
 
+    // Indicates if the Blitter interrupts has been triggered
+    bool birq;
+    
     
     //
     // Counters
@@ -243,6 +246,7 @@ public:
         & running
         & bbusy
         & bzero
+        & birq
 
         & remaining;
     }

Emulator/Agnus/FastBlitter.cpp
@@ -46,6 +46,7 @@ Blitter::beginFastLineBlit()
 
     // Terminate immediately
     signalEnd();
+    paula.raiseIrq(INT_BLIT);
     endBlit();
 }
 
@@ -61,6 +62,7 @@ Blitter::beginFastCopyBlit()
 
     // Terminate immediately
     signalEnd();
+    paula.raiseIrq(INT_BLIT);
     endBlit();
 }
 

Emulator/Agnus/SlowBlitter.cpp
@@ -970,7 +970,7 @@ Blitter::exec()
     }
 
     // Trigger Blitter interrupt if this is the termination cycle
-    if (instr & BLTDONE) paula.raiseIrq(INT_BLIT);
+    if ((instr & BLTDONE) && !birq) { paula.raiseIrq(INT_BLIT); birq = true; }
     
     // Allocate the bus if needed
     if (bus && !agnus.allocateBus<BUS_BLITTER>()) return;
@@ -1141,6 +1141,9 @@ Blitter::fakeExec()
         bus     = instr & (FETCH | BUS);
         busidle = instr & BUSIDLE;
     }
+
+    // Trigger Blitter interrupt if this is the termination cycle
+    if ((instr & BLTDONE) && !birq) { paula.raiseIrq(INT_BLIT); birq = true; }
 
     // Allocate the bus if needed
     if (bus && !agnus.allocateBus<BUS_BLITTER>()) return;