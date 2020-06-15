dirkwhoffmann/vAmiga

commit e44951a5738fb07f5fc5ce28fd7e1d6bd91f9ee9

cleanup

Emulator/CPU/Moira/MoiraDataflow_cpp.h
@@ -423,7 +423,7 @@ Moira::prefetch()
     queue.irc = readM<MEM_PROG, Word, F>(reg.pc + 2);
 }
 
-template<Flags F, int delay> void
+template<Flags F> void
 Moira::fullPrefetch()
 {    
     // Check for address error
@@ -433,7 +433,6 @@ Moira::fullPrefetch()
     }
 
     queue.irc = readM<MEM_PROG, Word>(reg.pc);
-    if (delay) sync(delay);
     prefetch<F>();
 }
 
@@ -468,7 +467,9 @@ Moira::jumpToVector(int nr)
     }
     
     // Update the prefetch queue
-    fullPrefetch <POLLIPL, 2> ();
-
+    queue.irc = readM<MEM_PROG, Word>(reg.pc);
+    sync(2);
+    prefetch<F>();
+    
     exceptionJump(nr, reg.pc);
 }

Emulator/CPU/Moira/MoiraDataflow.h
@@ -114,7 +114,7 @@ template<Flags F = 0> AEStackFrame makeFrame(u32 addr);
 template<Flags F = 0> void prefetch();
 
 // Performs a full prefetch cycle
-template<Flags F = 0, int delay = 0> void fullPrefetch();
+template<Flags F = 0> void fullPrefetch();
 
 // Reads an extension word from memory
 void readExt();

commit 93716abaaa0975c0307f1e2c4fcd08e3deea29e5

Fixed bug introduced in commit e44951a

Emulator/CPU/Moira/MoiraDataflow_cpp.h
@@ -469,7 +469,7 @@ Moira::jumpToVector(int nr)
     // Update the prefetch queue
     queue.irc = readM<MEM_PROG, Word>(reg.pc);
     sync(2);
-    prefetch<F>();
+    prefetch<POLLIPL>();
     
     exceptionJump(nr, reg.pc);
 }