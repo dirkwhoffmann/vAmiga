deb05df4116a13c879a1d936052e5ddf6f98568e
Dirk Hoffmann <dirk.hoffmann@me.com>
2020-06-28 13:07:02 +0200

    cleanup

--- GUI/Controller/MyController.swift
+++ GUI/Controller/MyController.swift
@@ -685,54 +685,6 @@ extension MyController {
     }
 
     //
-    // Dialogs
-    //
-    
-    //
-    // Keyboard events
-    //
-
-    // Keyboard events are handled by the emulator window.
-    // If they are handled here, some keys such as 'TAB' don't trigger an event.
-
-    //
-    //  Game pad events
-    //
-
-    // Feeds game pad actions into the Amiga's first control port
-    func emulateEventsOnGamePort1(_ events: [GamePadAction]) -> Bool {
-        
-        for event in events {
-            amiga.joystick1.trigger(event)
-            amiga.mouse.trigger(event)
-        }
-        return events != []
-    }
-    
-    // Feeds game pad actions into the Amiga's second control port
-    func emulateEventsOnGamePort2(_ events: [GamePadAction]) -> Bool {
-        
-        for event in events {
-            amiga.joystick2.trigger(event)
-            amiga.mouse.trigger(event)
-        }
-        return events != []
-    }
-
-    // Feeds game pad actions into the port associated with a certain game pad
-    @discardableResult
-    func emulateEventsOnGamePort(slot: Int, events: [GamePadAction]) -> Bool {
-        
-        if slot == config.gameDevice1 {
-            return emulateEventsOnGamePort1(events)
-        }
-        if slot == config.gameDevice2 {
-            return emulateEventsOnGamePort2(events)
-        }
-        return false
-    }
-    
-    //
     // Action methods (status bar)
     //
     
---
+++ GUI/Controller/MyControllerDevices.swift
@@ -0,0 +1,55 @@
+// -----------------------------------------------------------------------------
+// This file is part of vAmiga
+//
+// Copyright (C) Dirk W. Hoffmann. www.dirkwhoffmann.de
+// Licensed under the GNU General Public License v3
+//
+// See https://www.gnu.org for license information
+// -----------------------------------------------------------------------------
+
+extension MyController {
+    
+    //
+    // Keyboard events
+    //
+    
+    // Keyboard events are handled by the emulator window.
+    // If they are handled here, some keys such as 'TAB' don't trigger an event.
+    
+    //
+    //  Game port events
+    //
+    
+    // Feeds game pad actions into the port associated with a certain game pad
+    @discardableResult
+    func emulateEventsOnGamePort(slot: Int, events: [GamePadAction]) -> Bool {
+        
+        if slot == config.gameDevice1 {
+            return emulateEventsOnGamePort1(events)
+        }
+        if slot == config.gameDevice2 {
+            return emulateEventsOnGamePort2(events)
+        }
+        return false
+    }
+    
+    // Feeds game pad actions into the Amiga's first control port
+    func emulateEventsOnGamePort1(_ events: [GamePadAction]) -> Bool {
+        
+        for event in events {
+            amiga.joystick1.trigger(event)
+            amiga.mouse.trigger(event)
+        }
+        return events != []
+    }
+    
+    // Feeds game pad actions into the Amiga's second control port
+    func emulateEventsOnGamePort2(_ events: [GamePadAction]) -> Bool {
+        
+        for event in events {
+            amiga.joystick2.trigger(event)
+            amiga.mouse.trigger(event)
+        }
+        return events != []
+    }
+}
