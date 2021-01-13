f8f4dae22c9ae4aea137be5bb0a86483b10adbec
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-01-13 23:22:31 +0100

    Added OptionEnum, EmulatorStateEnum

--- Emulator/AmigaPublicTypes.h
+++ Emulator/AmigaPublicTypes.h
@@ -102,93 +102,21 @@ enum_long(OPT)
     OPT_AUDPAN,
     OPT_AUDVOL,
     OPT_AUDVOLL,
-    OPT_AUDVOLR
+    OPT_AUDVOLR,
+    
+    OPT_COUNT
 };
 typedef OPT Option;
-
-inline bool isOption(long value)
-{
-    return (unsigned long)value <= OPT_AUDVOLR;
-}
-
-inline const char *OptionName(Option value)
-{
-    switch (value) {
-            
-        case OPT_AGNUS_REVISION:      return "AGNUS_REVISION";
-        case OPT_SLOW_RAM_MIRROR:     return "SLOW_RAM_MIRROR";
-            
-        case OPT_DENISE_REVISION:     return "DENISE_REVISION";
-        case OPT_BRDRBLNK:            return "BRDRBLNK";
-            
-        case OPT_RTC_MODEL:           return "RTC_MODEL";
-
-        case OPT_CHIP_RAM:            return "CHIP_RAM";
-        case OPT_SLOW_RAM:            return "SLOW_RAM";
-        case OPT_FAST_RAM:            return "FAST_RAM";
-        case OPT_EXT_START:           return "EXT_START";
-        case OPT_SLOW_RAM_DELAY:      return "SLOW_RAM_DELAY";
-        case OPT_BANKMAP:             return "BANKMAP";
-        case OPT_UNMAPPING_TYPE:      return "UNMAPPING_TYPE";
-        case OPT_RAM_INIT_PATTERN:    return "RAM_INIT_PATTERN";
-            
-        case OPT_DRIVE_CONNECT:       return "DRIVE_CONNECT";
-        case OPT_DRIVE_SPEED:         return "DRIVE_SPEED";
-        case OPT_LOCK_DSKSYNC:        return "LOCK_DSKSYNC";
-        case OPT_AUTO_DSKSYNC:        return "AUTO_DSKSYNC";
-
-        case OPT_DRIVE_TYPE:          return "DRIVE_TYPE";
-        case OPT_EMULATE_MECHANICS:   return "EMULATE_MECHANICS";
-            
-        case OPT_SERIAL_DEVICE:       return "SERIAL_DEVICE";
-
-        case OPT_HIDDEN_SPRITES:      return "HIDDEN_SPRITES";
-        case OPT_HIDDEN_LAYERS:       return "HIDDEN_LAYERS";
-        case OPT_HIDDEN_LAYER_ALPHA:  return "HIDDEN_LAYER_ALPHA";
-        case OPT_CLX_SPR_SPR:         return "CLX_SPR_SPR";
-        case OPT_CLX_SPR_PLF:         return "CLX_SPR_PLF";
-        case OPT_CLX_PLF_PLF:         return "CLX_PLF_PLF";
-                
-        case OPT_BLITTER_ACCURACY:    return "BLITTER_ACCURACY";
-            
-        case OPT_CIA_REVISION:        return "CIA_REVISION";
-        case OPT_TODBUG:              return "TODBUG";
-        case OPT_ECLOCK_SYNCING:      return "ECLOCK_SYNCING";
-        case OPT_ACCURATE_KEYBOARD:   return "ACCURATE_KEYBOARD";
-            
-        case OPT_SAMPLING_METHOD:     return "SAMPLING_METHOD";
-        case OPT_FILTER_TYPE:         return "FILTER_TYPE";
-        case OPT_FILTER_ALWAYS_ON:    return "FILTER_ALWAYS_ON";
-        case OPT_AUDPAN:              return "AUDPAN";
-        case OPT_AUDVOL:              return "AUDVOL";
-        case OPT_AUDVOLL:             return "AUDVOLL";
-        case OPT_AUDVOLR:             return "AUDVOLR";
-    }
-    return "???";
-}
 
 enum_long(EMULATOR_STATE)
 {
     EMULATOR_STATE_OFF,
     EMULATOR_STATE_PAUSED,
-    EMULATOR_STATE_RUNNING
+    EMULATOR_STATE_RUNNING,
+
+    EMULATOR_STATE_COUNT
 };
 typedef EMULATOR_STATE EmulatorState;
-
-inline bool isEmulatorState(long value) {
-    return (unsigned long)value <=  EMULATOR_STATE_RUNNING;
-}
-
-inline const char *EmulatorStateName(EmulatorState value)
-{
-    switch (value) {
-            
-        case EMULATOR_STATE_OFF:      return "OFF";
-        case EMULATOR_STATE_PAUSED:   return "PAUSED";
-        case EMULATOR_STATE_RUNNING:  return "RUNNING";
-    }
-    return "???";
-}
 
 enum_long(ErrorCode)
 {
