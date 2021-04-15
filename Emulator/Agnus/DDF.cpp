4d63b5bdd392e94f6255c6d930b8709658378b3a
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-04-15 16:43:48 +0200

    cleanup

--- Emulator/Agnus/DDF.cpp
+++ Emulator/Agnus/DDF.cpp
@@ -14,16 +14,6 @@
 template <bool hires> void
 DDF<hires>::compute(i16 ddfstrt, i16 ddfstop)
 {
-    compute(strtEven, stopEven, ddfstrt, ddfstop);
-    compute(strt,  stop,  ddfstrt, ddfstop);
-    
-    assert(strtEven == strt);
-    assert(stopEven == stop);
-}
-
-template <bool hires> void
-DDF<hires>::compute(i16 &strt, i16 &stop, i16 ddfstrt, i16 ddfstop)
-{
     if (hires) {
                
         // Compute the beginning of the fetch window
--- Emulator/Agnus/DDF.h
+++ Emulator/Agnus/DDF.h
@@ -16,19 +16,11 @@ struct DDF
 {
     // First bitplane DMA cycle of odd and even bitplanes
     i16 strt;
-
-private:
-    i16 strtEven;
-public:
     
     // Last bitplane DMA cycle + 1 of odd and even bitplanes
     i16 stop;
 
-private:
-    i16 stopEven;
-public:
-
-    DDF() : strt(0), strtEven(0), stop(0), stopEven(0) { }
+    DDF() : strt(0), stop(0) { }
     
     template <class W>
     void operator<<(W& worker)
@@ -36,12 +28,10 @@ public:
         worker
         
         << strt
-        << strtEven
-        << stop
-        << stopEven;
+        << stop;
     }
     
-    void clear() { strt = strtEven = stop = stopEven = 0; }
+    void clear() { strt = stop = 0; }
     
     bool operator==(const DDF& ddf) const
     {
@@ -65,5 +55,4 @@ public:
      * are not yet supported by the emulator.
      */
     void compute(i16 ddfstrt, i16 ddfstop);
-    void compute(i16 &strt, i16 &stop, i16 ddfstrt, i16 ddfstop);
 };
