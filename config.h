cd649945e986f415ee9ddc42c6c72d8da363d314
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-04-20 07:23:40 +0200

    This is vAmiga 1.0 beta 1

--- config.h
+++ config.h
@@ -16,7 +16,7 @@
 // Snapshot version number
 #define V_MAJOR 0
 #define V_MINOR 9
-#define V_SUBMINOR 18
+#define V_SUBMINOR 19
 
 // Uncomment these settings in a release build
 // #define RELEASEBUILD
