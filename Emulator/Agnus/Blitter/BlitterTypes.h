dae947ad5226960cdc10273056d348d55b8ba773
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-10-24 16:43:01 +0200

    cleanup

--- Emulator/Agnus/Blitter/BlitterTypes.h
+++ Emulator/Agnus/Blitter/BlitterTypes.h
@@ -12,6 +12,42 @@
 #include "Aliases.h"
 
 //
+// Bit masks
+//
+
+#ifdef __cplusplus
+
+// BLTCON0 (area mode)
+constexpr u16 BLTCON0_ASH     = 0xF000;
+constexpr u16 BLTCON0_USEA    = 0x0800;
+constexpr u16 BLTCON0_USEB    = 0x0400;
+constexpr u16 BLTCON0_USEC    = 0x0200;
+constexpr u16 BLTCON0_USED    = 0x0100;
+constexpr u16 BLTCON0_LF      = 0x00FF;
+
+// BLTCON0 (line mode)
+constexpr u16 BLTCON0_START   = 0xF000;
+
+// BLTCON1 (area mode)
+constexpr u16 BLTCON1_BSH     = 0xF000;
+constexpr u16 BLTCON1_DOFF    = 0x0080;
+constexpr u16 BLTCON1_EFE     = 0x0010;
+constexpr u16 BLTCON1_IFE     = 0x0008;
+constexpr u16 BLTCON1_FCI     = 0x0004;
+constexpr u16 BLTCON1_DESC    = 0x0002;
+constexpr u16 BLTCON1_LINE    = 0x0001;
+
+// BLTCON1 (line mode)
+constexpr u16 BLTCON1_TEXTURE = 0xF000;
+constexpr u16 BLTCON1_SIGN    = 0x0040;
+constexpr u16 BLTCON1_SUD     = 0x0010;
+constexpr u16 BLTCON1_SUL     = 0x0008;
+constexpr u16 BLTCON1_AUL     = 0x0004;
+constexpr u16 BLTCON1_SING    = 0x0002;
+
+#endif
+
+//
 // Structures
 //
 
