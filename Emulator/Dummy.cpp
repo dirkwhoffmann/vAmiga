0374077a2ee5163bf29d551e004cb2d51191a290
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-11-23 18:16:54 +0100

    Added experimental CMake files

---
+++ Emulator/Dummy.cpp
@@ -0,0 +1,19 @@
+//
+//  Dummy.cpp
+//  vAmiga
+//
+//  Created by Dirk Hoffmann on 23.11.21.
+//  Copyright © 2021 Dirk Hoffmann. All rights reserved.
+//
+
+#include <stdio.h>
+#include "config.h"
+// #include "Chrono.h"
+
+int main(int argc, char *argv[])
+{
+//    auto now = util::Time::now();
+  //
+    // printf("%f\n", now.asSeconds());
+    printf("Hallo\n");
+}
