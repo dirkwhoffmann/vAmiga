ebb968eccbf55f0e1066b2c0a612e5727df8e428
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-11-30 13:01:39 +0100

    Reverting to M_PI instead of std::numbers::pi

--- Emulator/Paula/Audio/AudioFilter.cpp
+++ Emulator/Paula/Audio/AudioFilter.cpp
@@ -10,7 +10,6 @@
 #include "config.h"
 #include "AudioFilter.h"
 #include <cmath>
-#include <numbers>
 
 void
 AudioFilter::setSampleRate(double sampleRate)
@@ -28,7 +27,7 @@ AudioFilter::setSampleRate(double sampleRate)
     const double ff = f_cutoff / sampleRate;
     
     // Compute coefficients
-    const double ita = 1.0/ tan(std::numbers::pi * ff);
+    const double ita = 1.0/ tan(M_PI * ff);
     const double q = sqrt(2.0);
     
     b0 = 1.0 / (1.0 + q * ita + ita * ita);
