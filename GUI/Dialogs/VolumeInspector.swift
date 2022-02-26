b123ecb9594554280804de9e9166e35281202839
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-02-26 11:30:32 +0100

    Work on VolumeInspector

--- GUI/Dialogs/VolumeInspector.swift
+++ GUI/Dialogs/VolumeInspector.swift
@@ -22,18 +22,6 @@ class VolumeInspector: DialogController {
 
     @IBOutlet weak var previewScrollView: NSScrollView!
     @IBOutlet weak var previewTable: NSTableView!
-    @IBOutlet weak var cylinderText: NSTextField!
-    @IBOutlet weak var cylinderField: NSTextField!
-    @IBOutlet weak var cylinderStepper: NSStepper!
-    @IBOutlet weak var headText: NSTextField!
-    @IBOutlet weak var headField: NSTextField!
-    @IBOutlet weak var headStepper: NSStepper!
-    @IBOutlet weak var trackText: NSTextField!
-    @IBOutlet weak var trackField: NSTextField!
-    @IBOutlet weak var trackStepper: NSStepper!
-    @IBOutlet weak var sectorText: NSTextField!
-    @IBOutlet weak var sectorField: NSTextField!
-    @IBOutlet weak var sectorStepper: NSStepper!
     @IBOutlet weak var blockText: NSTextField!
     @IBOutlet weak var blockField: NSTextField!
     @IBOutlet weak var blockStepper: NSStepper!
@@ -51,24 +39,18 @@ class VolumeInspector: DialogController {
     var dfn: DriveProxy { return amiga.df(nr)! }
     var dhn: HardDriveProxy { return amiga.dh(nr)! }
     
-    // The drive geometry (cylinders, heads, sectors, tracks, blocks)
-    var c = 0
-    var h = 0
-    var s = 0
-    var t = 0
+    // Number of blocks in this volume (DEPRECATED)
     var b = 0
     
-    var upperCyl: Int { return c > 0 ? c - 1 : 0 }
-    var upperHead: Int { return h > 0 ? h - 1 : 0 }
-    var upperSector: Int { return s > 0 ? s - 1 : 0 }
-    var upperTrack: Int { return t > 0 ? t - 1 : 0 }
     var upperBlock: Int { return b > 0 ? b - 1 : 0 }
 
     // Results of the different decoders
     var hdf: HDFFileProxy?
     var adf: ADFFileProxy?
+    /*
     var img: IMGFileProxy?
     var ext: EXTFileProxy?
+    */
     var vol: FSDeviceProxy?
     
     // Result of the consistency checker
@@ -81,21 +63,11 @@ class VolumeInspector: DialogController {
 
     var numPartitions: Int { return hdf?.numPartitions ?? 1 }
 
-    var isDD: Bool {
-        return adf?.isDD ?? img?.isDD ?? ext?.isDD ?? false
-    }
-    var isHD: Bool {
-        return adf?.isHD ?? img?.isHD ?? ext?.isHD ?? false
-    }
     var hasVirus: Bool {
         return adf?.hasVirus ?? false
     }
 
     // Block preview
-    var cylinderNr = 0
-    var headNr = 0
-    var trackNr = 0
-    var sectorNr = 0
     var blockNr = 0
     
     //
@@ -110,14 +82,8 @@ class VolumeInspector: DialogController {
 
         // Run the ADF decoder
         adf = try? ADFFileProxy.make(drive: dfn) as ADFFileProxy
-
-        // Run the extended ADF decoder
-        ext = try? EXTFileProxy.make(drive: dfn) as EXTFileProxy
-
-        // Run the DOS decoder
-        img = try? IMGFileProxy.make(drive: dfn) as IMGFileProxy
                         
-        // Try to decide the file system
+        // Try to extract the file system
         initVolume(partition: 0)
                 
         super.showSheet()
@@ -146,36 +112,10 @@ class VolumeInspector: DialogController {
         if adf != nil { vol = try? FSDeviceProxy.make(withADF: adf!) }
         if hdf != nil { vol = try? FSDeviceProxy.make(withHDF: hdf!, partition: nr) }
         
-        initDriveGeometry()
-    }
-    
-    func initDriveGeometry() {
-        
-        // Read CHS geometry (cylinders, heads, sectors)
-        if vol != nil {
-            c = vol!.numCyls; h = vol!.numSectors; s = vol!.numSectors
-        } else if hdf != nil {
-            c = hdf!.numCyls; h = hdf!.numSectors; s = hdf!.numSectors
-        } else if adf != nil {
-            c = adf!.numCyls; h = adf!.numSectors; s = adf!.numSectors
-        } else if img != nil {
-            c = img!.numCyls; h = img!.numSectors; s = img!.numSectors
-        } else if ext != nil {
-            c = ext!.numCyls; h = ext!.numSectors; s = ext!.numSectors
-        }
-        
-        // Update derived values (tracks, blocks)
-        t = c * h
-        b = t * s
-        
-        // Keep the current selection in the valid range
-        cylinderNr = cylinderNr.clamped(0, upperCyl)
-        sectorNr = sectorNr.clamped(0, upperSector)
-        headNr = headNr.clamped(0, upperHead)
-        trackNr = trackNr.clamped(0, upperTrack)
+        b = vol?.numBlocks ?? 0
         blockNr = blockNr.clamped(0, upperBlock)
     }
-    
+        
     override public func awakeFromNib() {
         
         track()
@@ -188,7 +128,6 @@ class VolumeInspector: DialogController {
         partitionPopup.isHidden = numPartitions == 1
         
         // Configure elements
-        sectorStepper.maxValue = .greatestFiniteMagnitude
         blockStepper.maxValue = .greatestFiniteMagnitude
         
         // Run a file system check
@@ -240,14 +179,6 @@ class VolumeInspector: DialogController {
         strictButton.isHidden = vol == nil
         
         // Update all elements
-        cylinderField.stringValue      = String(format: "%d", cylinderNr)
-        cylinderStepper.integerValue   = cylinderNr
-        headField.stringValue          = String(format: "%d", headNr)
-        headStepper.integerValue       = headNr
-        trackField.stringValue         = String(format: "%d", trackNr)
-        trackStepper.integerValue      = trackNr
-        sectorField.stringValue        = String(format: "%d", sectorNr)
-        sectorStepper.integerValue     = sectorNr
         blockField.stringValue         = String(format: "%d", blockNr)
         blockStepper.integerValue      = blockNr
         corruptionStepper.integerValue = blockNr
@@ -277,22 +208,8 @@ class VolumeInspector: DialogController {
     
     func updateDiskIcon() {
 
-        if hdf != nil {
-            
-            icon.image = NSImage(named: "hdf")!
-            virus.isHidden = true
-            decontaminationButton.isHidden = true
-            return
-        }
-            
-        var name = ""
-            
-        if ext != nil { name = isHD ? "hd_other" : "dd_other" }
-        if adf != nil { name = isHD ? "hd_adf" : "dd_adf" }
-        if img != nil { name = "dd_dos" }
-        
-        if name != "" && dfn.hasWriteProtectedDisk() { name += "_protected" }
-        
+        let name = ""
+    
         icon.image = NSImage(named: name != "" ? name : "biohazard")
         virus.isHidden = !hasVirus
         decontaminationButton.isHidden = !hasVirus
@@ -300,52 +217,18 @@ class VolumeInspector: DialogController {
     
     func updateTitleText() {
         
-        var text = "Raw MFM stream"
-        var color = NSColor.textColor
-        
-        if hdf != nil {
-            
-            text = "Amiga Hard Drive"
-            color = .textColor
-            
-        } else if adf != nil {
-            
-            text = "Amiga Disk"
-            color = .textColor
+        let text = "Amiga File System"
+        let color = NSColor.textColor
                 
-        } else if img != nil {
-                
-            text = "PC Disk"
-            color = .textColor
-        }
-        
         title.stringValue = text
         title.textColor = color
     }
 
     func updateTrackAndSectorInfo() {
         
-        var text = "Unknown track and sector format"
-        var color = NSColor.warningColor
+        let text = "Lorem ipsum"
+        let color = NSColor.warningColor
         
-        if hdf != nil {
-            
-            let blocks = vol!.numBlocks
-            let capacity = blocks / 2000
-            text = "\(capacity) MB (\(blocks) sectors)"
-            color = NSColor.secondaryLabelColor
-            
-        } else if adf != nil {
-            
-            text = adf!.layoutInfo
-            color = NSColor.secondaryLabelColor
-            
-        } else if adf != nil {
-            
-            text = img!.layoutInfo
-            color = NSColor.secondaryLabelColor
-        }
-
         layoutInfo.stringValue = text
         layoutInfo.textColor = color
     }
@@ -428,78 +311,11 @@ class VolumeInspector: DialogController {
     // Helper methods
     //
     
-    func setCylinder(_ newValue: Int) {
-        
-        if newValue != cylinderNr {
-
-            let value = newValue.clamped(0, upperCyl)
-
-            cylinderNr = value
-            trackNr    = cylinderNr * 2 + headNr
-            blockNr    = trackNr * s + sectorNr
-            
-            selection = nil
-            update()
-        }
-    }
-    
-    func setHead(_ newValue: Int) {
-        
-        if newValue != headNr {
-                        
-            let value = newValue.clamped(0, upperHead)
-
-            headNr     = value
-            trackNr    = cylinderNr * 2 + headNr
-            blockNr    = trackNr * s + sectorNr
-            
-            selection = nil
-            update()
-        }
-    }
-    
-    func setTrack(_ newValue: Int) {
-        
-        if newValue != trackNr {
-                   
-            let value = newValue.clamped(0, upperTrack)
-            
-            trackNr    = value
-            cylinderNr = trackNr / 2
-            headNr     = trackNr % 2
-            blockNr    = trackNr * s + sectorNr
-
-            selection = nil
-            update()
-        }
-    }
-
-    func setSector(_ newValue: Int) {
-        
-        if newValue != sectorNr {
-                  
-            let value = newValue.clamped(0, upperSector)
-            
-            sectorNr   = value
-            blockNr    = trackNr * s + sectorNr
-            
-            selection = nil
-            update()
-        }
-    }
-
     func setBlock(_ newValue: Int) {
         
         if newValue != blockNr {
                         
-            let value = newValue.clamped(0, upperBlock)
-
-            blockNr    = value
-            trackNr    = blockNr / s
-            sectorNr   = blockNr % s
-            cylinderNr = trackNr / 2
-            headNr     = trackNr % 2
-            
+            blockNr = newValue.clamped(0, upperBlock)
             selection = nil
             update()
         }
@@ -548,46 +364,6 @@ class VolumeInspector: DialogController {
             update()
         }
     }
-
-    @IBAction func cylinderAction(_ sender: NSTextField!) {
-        
-        setCylinder(sender.integerValue)
-    }
-    
-    @IBAction func cylinderStepperAction(_ sender: NSStepper!) {
-        
-        setCylinder(sender.integerValue)
-    }
-    
-    @IBAction func headAction(_ sender: NSTextField!) {
-        
-        setHead(sender.integerValue)
-    }
-    
-    @IBAction func headStepperAction(_ sender: NSStepper!) {
-        
-        setHead(sender.integerValue)
-    }
-    
-    @IBAction func trackAction(_ sender: NSTextField!) {
-        
-        setTrack(sender.integerValue)
-    }
-    
-    @IBAction func trackStepperAction(_ sender: NSStepper!) {
-        
-        setTrack(sender.integerValue)
-    }
-    
-    @IBAction func sectorAction(_ sender: NSTextField!) {
-        
-        setSector(sender.integerValue)
-    }
-    
-    @IBAction func sectorStepperAction(_ sender: NSStepper!) {
-        
-        setSector(sender.integerValue)
-    }
     
     @IBAction func blockAction(_ sender: NSTextField!) {
         
@@ -609,18 +385,6 @@ class VolumeInspector: DialogController {
         track()
         errorReport = vol?.check(strict)
         update()
-    }
-    
-    @IBAction override func okAction(_ sender: Any!) {
-        
-        track()
-        hideSheet()
-    }
-    
-        @IBAction override func cancelAction(_ sender: Any!) {
-        
-        track()
-        hideSheet()
     }
 }
 
@@ -666,12 +430,7 @@ extension VolumeInspector: NSTableViewDataSource {
             if let byte = vol?.readByte(blockNr, offset: 16 * row + col) {
                 return String(format: "%02X", byte)
             }
-            if let byte = adf?.readByte(blockNr, offset: 16 * row + col) {
-                return String(format: "%02X", byte)
-            }
-            if let byte = img?.readByte(blockNr, offset: 16 * row + col) {
-                return String(format: "%02X", byte)
-            }
+
         } else {
             return String(format: "%X", row)
         }
