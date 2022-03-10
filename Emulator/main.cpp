889a981636b0981c905c6e3269f88cad3c6c3a1b
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-03-10 12:44:34 +0100

    Added second build target for headless core

---
+++ Emulator/main.cpp
@@ -0,0 +1,20 @@
+// -----------------------------------------------------------------------------
+// This file is part of vAmiga
+//
+// Copyright (C) Dirk W. Hoffmann. www.dirkwhoffmann.de
+// Licensed under the GNU General Public License v3
+//
+// See https://www.gnu.org for license information
+// -----------------------------------------------------------------------------
+
+#include "config.h"
+#include "Amiga.h"
+
+// Callback process(...)
+
+int main(int argc, char *argv[])
+{
+    Amiga amiga;
+    
+    printf("Hello, vAmiga\n");
+}
