4f6362590126cf10aee8e625846fc1d1a320ea22
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-04-10 11:24:50 +0200

    This is vAmiga v2.0b3

--- Emulator/config.h
+++ Emulator/config.h
@@ -17,16 +17,16 @@
 #define VER_MAJOR 2
 #define VER_MINOR 0
 #define VER_SUBMINOR 0
-#define VER_BETA 2
+#define VER_BETA 3
 
 // Snapshot version number
 #define SNP_MAJOR 2
 #define SNP_MINOR 0
 #define SNP_SUBMINOR 0
-#define SNP_BETA 2
+#define SNP_BETA 3
 
 // Uncomment this setting in a release build
-// #define RELEASEBUILD
+#define RELEASEBUILD
 
 //
 // Build settings
