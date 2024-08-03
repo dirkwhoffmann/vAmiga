4c2144b3b29025097e9a157626ced9c883d5ce8e
Dirk Hoffmann <dirk.hoffmann@me.com>
2024-08-03 10:35:38 +0200

    cleanup

--- Emulator/Base/CoreObject.h
+++ Emulator/Base/CoreObject.h
@@ -18,14 +18,7 @@ class CoreObject : public Dumpable {
 
 protected:
 
-    /* Verbosity level.
-     *
-     * The variable controls the amount and overall format of generated debug
-     * output. If set to zero, all debug messages are omitted. If set to a
-     * value of 1 or above, the debug message is prefixed with additional
-     * information about the emulator state, such as the component name issuing
-     * the message, the currently processed frame, or the value of CPU flags.
-     */
+    // Verbosity level
     static isize verbosity;
 
     
@@ -55,14 +48,17 @@ public:
  *   - debug  Debug message         (Shows up in debug builds, only)
  *   - trace  Detailed debug output (Shows up in debug builds, only)
  *
- * Debug messages are prefixed by the component name and a line number. Trace
- * messages are prefixed by a more detailed string description produced by the
- * prefix() function.
- *
  * Debug, plain, and trace messages are accompanied by an optional 'enable'
  * parameter. If 0 is passed in, no output will be generated. In addition,
  * variable 'verbose' is checked which is set to true by default. By setting
  * this variable to false, debug output can be silenced temporarily.
+ *
+ * Debug messages are also affected by the verbosity level which is a static
+ * member of the CoreComponent class. If set to 0, all debug messages are
+ * omitted. If set the 1, debug messages appear as plain text. If set to a
+ * value of 1 or above, the debug message is prefixed with additional
+ * information about the emulator state, such as the component name issuing
+ * the message, the currently processed frame, or the value of CPU flags.
  *
  * Sidenote: In previous releases the printing macros were implemented in form
  * of variadic functions. Although this might seem to be superior at first
@@ -80,17 +76,15 @@ fprintf(stderr, "Warning: " format __VA_OPT__(,) __VA_ARGS__);
 #define fatal(format, ...) \
 { fprintf(stderr, "Fatal: " format __VA_OPT__(,) __VA_ARGS__); exit(1); }
 
-#define debugmsg(enable, level, format, ...) \
+#define debug(enable, format, ...) \
 if (enable) { if (verbosity) { \
-prefix(level, __LINE__); \
+prefix(verbosity, __LINE__); \
 fprintf(stderr, format __VA_OPT__(,) __VA_ARGS__); }}
 
-#define debug(enable, format, ...) \
-debugmsg(enable, verbosity, format __VA_OPT__(,) __VA_ARGS__);
-
-// DEPRECATED
 #define trace(enable, format, ...) \
-debugmsg(enable, 5, format __VA_OPT__(,) __VA_ARGS__);
+if (enable) { if (verbosity) { \
+prefix(5, __LINE__); \
+fprintf(stderr, format __VA_OPT__(,) __VA_ARGS__); }}
 
 #define xfiles(format, ...) \
 if (XFILES) { if (verbosity) { \
