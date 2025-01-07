a3fe74d723d30d9ec48289faebd839f871ae7b7d
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-01-07 15:35:36 +0100

    Work on Dashboard

--- GUI/Dialogs/Dashboard/DashboardPanel.swift
+++ GUI/Dialogs/Dashboard/DashboardPanel.swift
@@ -57,92 +57,92 @@ class DashboardPanel: NSView {
     }
 }
 
-class TimeLinePanel: DashboardPanel {
+struct TimeSeriesContentView: View {
     
-    var host: NSHostingView<ContentView>!
+    @ObservedObject var model: DashboardDataProvider
+    var panel: DashboardPanel!
     
-    struct ContentView: View {
+    var body: some View {
         
-        @ObservedObject var model: DashboardDataProvider
-        var panel: DashboardPanel!
-        
-        var body: some View {
+        VStack(alignment: .leading) {
             
             VStack(alignment: .leading) {
-                
-                VStack(alignment: .leading) {
-                    Text(panel.heading)
-                        .font(.system(size: 14))
-                        .fontWeight(.bold)
-                        .foregroundColor(panel.headingColor)
-                        .padding(.bottom, 1)
-                    Text(panel.subHeading)
-                        .font(.system(size: 8))
-                        .fontWeight(.regular)
-                        .foregroundColor(panel.subheadingColor)
-                }
-                .padding(EdgeInsets(top: 10, leading: 15, bottom: 5, trailing: 15))
-                
-                Chart {
-                    
-                    ForEach(model.data.filter { $0.series != 3 }, id: \.id) { dataPoint in
-                        AreaMark(
-                            x: .value("Time", dataPoint.timestamp),
-                            y: .value("Value", dataPoint.value),
-                            series: .value("Series", dataPoint.series)
-                        )
-                        .foregroundStyle(by: .value("Series", dataPoint.series))
-                    }
-                    
-                    ForEach(model.data.filter { $0.series == 3 }, id: \.id) { dataPoint in
-                        LineMark(
-                            x: .value("Time", dataPoint.timestamp),
-                            y: .value("Value", dataPoint.value),
-                            series: .value("Series", dataPoint.series)
-                        )
-                        
-                        .interpolationMethod(.catmullRom)
-                        .foregroundStyle(panel.lineColor)
-                        .lineStyle(StrokeStyle(lineWidth: 1.25))
-                        /*
-                        .symbol {
-                            if #available(macOS 15.0, *) {
-                                Circle()
-                                    .fill(graph1Color.mix(with: .white, by: 0.5))
-                                    .frame(width: 2)
-                            } else {
-                                Circle()
-                                    .fill(graph1Color)
-                                    .frame(width: 2)
-                            }
-                        }
-                        */
-                    }
-                }
-                .chartXScale(domain: Date() - DashboardDataProvider.maxTimeSpan...Date())
-                .chartXAxis(.hidden)
-                .chartYScale(domain: model.range)
-                .chartYAxis {
-                    AxisMarks(values: model.gridLines) {
-                        AxisGridLine()
-                            .foregroundStyle(panel.gridLineColor)
-                    }
-                }
-                .padding(EdgeInsets(top: 0.0, leading: 15.0, bottom: 15.0, trailing: 15.0))
-                .chartLegend(.hidden)
-                .chartForegroundStyleScale(panel.gradients)
+                Text(panel.heading)
+                    .font(.system(size: 14))
+                    .fontWeight(.bold)
+                    .foregroundColor(panel.headingColor)
+                    .padding(.bottom, 1)
+                Text(panel.subHeading)
+                    .font(.system(size: 8))
+                    .fontWeight(.regular)
+                    .foregroundColor(panel.subheadingColor)
             }
-            .background(panel.background)
-            .cornerRadius(10)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(EdgeInsets(top: 10, leading: 15, bottom: 5, trailing: 15))
+            
+            Chart {
+                
+                ForEach(model.data.filter { $0.series != 3 }, id: \.id) { dataPoint in
+                    AreaMark(
+                        x: .value("Time", dataPoint.timestamp),
+                        y: .value("Value", dataPoint.value),
+                        series: .value("Series", dataPoint.series)
+                    )
+                    .foregroundStyle(by: .value("Series", dataPoint.series))
+                }
+                
+                ForEach(model.data.filter { $0.series == 3 }, id: \.id) { dataPoint in
+                    LineMark(
+                        x: .value("Time", dataPoint.timestamp),
+                        y: .value("Value", dataPoint.value),
+                        series: .value("Series", dataPoint.series)
+                    )
+                    
+                    .interpolationMethod(.catmullRom)
+                    .foregroundStyle(panel.lineColor)
+                    .lineStyle(StrokeStyle(lineWidth: 1.25))
+                    /*
+                    .symbol {
+                        if #available(macOS 15.0, *) {
+                            Circle()
+                                .fill(graph1Color.mix(with: .white, by: 0.5))
+                                .frame(width: 2)
+                        } else {
+                            Circle()
+                                .fill(graph1Color)
+                                .frame(width: 2)
+                        }
+                    }
+                    */
+                }
+            }
+            .chartXScale(domain: Date() - DashboardDataProvider.maxTimeSpan...Date())
+            .chartXAxis(.hidden)
+            .chartYScale(domain: model.range)
+            .chartYAxis {
+                AxisMarks(values: model.gridLines) {
+                    AxisGridLine()
+                        .foregroundStyle(panel.gridLineColor)
+                }
+            }
+            .padding(EdgeInsets(top: 0.0, leading: 15.0, bottom: 15.0, trailing: 15.0))
+            .chartLegend(.hidden)
+            .chartForegroundStyleScale(panel.gradients)
         }
+        .background(panel.background)
+        .cornerRadius(10)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+}
+
+class TimeLinePanel: DashboardPanel {
+    
+    var host: NSHostingView<TimeSeriesContentView>!
     
     required init?(coder aDecoder: NSCoder) {
         
         super.init(coder: aDecoder)
         
-        host = NSHostingView(rootView: ContentView( model: model, panel: self))
+        host = NSHostingView(rootView: TimeSeriesContentView( model: model, panel: self))
         self.addSubview(host)
         
         host.translatesAutoresizingMaskIntoConstraints = false
@@ -157,121 +157,145 @@ class TimeLinePanel: DashboardPanel {
 // Gauge view
 //
 
-class GaugePanel: DashboardPanel {
+struct GaugeContentView: View {
     
-    var host: NSHostingView<ContentView>!
+    @ObservedObject var model: DashboardDataProvider
+    var panel: DashboardPanel
+    
+    private var themeColor: Color {
+        return Color(nsColor: panel.themeColor)
+    }
+    private var graph1Color: Color {
+        return panel.graph1Color
+    }
+    private var graph2Color: Color {
+        return panel.graph2Color
+    }
+    private var background: Gradient {
+        return Gradient(colors: [Color.black, Color.black])
+    }
+    private var gradients: KeyValuePairs<Int, Gradient> {
+        return [ 1: Gradient(colors: [graph1Color.opacity(0.75), graph1Color.opacity(0.25)]),
+                 2: Gradient(colors: [graph2Color.opacity(0.75), graph2Color.opacity(0.25)])]
+    }
+    private var lineColor: Color {
+        
+        if #available(macOS 15.0, *) {
+            return graph1Color.mix(with: .white, by: 0.25)
+        } else {
+            return graph1Color
+        }
+    }
+    private var gridLineColor: Color {
+        return Color.white.opacity(0.6)
+    }
+    private var lineWidth: Double {
+        return 1.25 // 0.75
+    }
 
-    struct ContentView: View {
+    let gradient = Gradient(colors: [.green, .yellow, .orange, .red])
+    
+    var body: some View {
         
-        @ObservedObject var model: DashboardDataProvider
-        var panel: DashboardPanel
-        
-        private var themeColor: Color {
-            return Color(nsColor: panel.themeColor)
-        }
-        private var graph1Color: Color {
-            return panel.graph1Color
-        }
-        private var graph2Color: Color {
-            return panel.graph2Color
-        }
-        private var background: Gradient {
-            return Gradient(colors: [Color.black, Color.black])
-        }
-        private var gradients: KeyValuePairs<Int, Gradient> {
-            return [ 1: Gradient(colors: [graph1Color.opacity(0.75), graph1Color.opacity(0.25)]),
-                     2: Gradient(colors: [graph2Color.opacity(0.75), graph2Color.opacity(0.25)])]
-        }
-        private var lineColor: Color {
+        GeometryReader { geometry in
             
-            if #available(macOS 15.0, *) {
-                return graph1Color.mix(with: .white, by: 0.25)
-            } else {
-                return graph1Color
-            }
-        }
-        private var gridLineColor: Color {
-            return Color.white.opacity(0.6)
-        }
-        private var lineWidth: Double {
-            return 1.25 // 0.75
-        }
-
-        let gradient = Gradient(colors: [.green, .yellow, .orange, .red])
-        
-        var body: some View {
+            ZStack() {
             
-            GeometryReader { geometry in
-                
-                ZStack() {
-                
-                    // Gauge
-                    VStack {
-                        HStack {
-                            Spacer()
-                            if #available(macOS 14.0, *) {
-                                
-                                Gauge(value: model.latest(), in: model.range) {
-                                    Text("")
-                                } currentValueLabel: {
-                                    Text(String(format: "%.2f", model.latest()))
-                                    //                                Text(Double(model.val()), format: .number)
-                                }
-                                .gaugeStyle(.accessoryCircular)
-                                .tint(gradient)
-                                .scaleEffect(1.4)
-                                .frame(width: 100, height: 100)
-                                // .padding(0)
-                                // .background(.green)
-                                
-                            } else { }
-                        }
-                    }
-                    
-                    VStack {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                
-                                Text(panel.heading)
-                                    .font(.system(size: 14))
-                                    .fontWeight(.bold)
-                                    .foregroundColor(themeColor.opacity(1.0))
-                                    .padding(.bottom, 1)
-                                Text(panel.subHeading)
-                                    .font(.system(size: 8))
-                                    .fontWeight(.regular)
-                                    .foregroundColor(Color.gray)
-                                    .padding(.bottom, 1)
+                // Gauge
+                VStack {
+                    HStack {
+                        Spacer()
+                        if #available(macOS 14.0, *) {
+                            
+                            Gauge(value: model.latest(), in: model.range) {
+                                Text("")
+                            } currentValueLabel: {
+                                Text(String(format: "%.2f", model.latest()))
+                                //                                Text(Double(model.val()), format: .number)
                             }
-                            Spacer()
+                            .gaugeStyle(.accessoryCircular)
+                            .tint(gradient)
+                            .scaleEffect(1.4)
+                            .frame(width: 100, height: 100)
+                            // .padding(0)
+                            // .background(.green)
+                            
+                        } else { }
+                    }
+                }
+                
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            
+                            Text(panel.heading)
+                                .font(.system(size: 14))
+                                .fontWeight(.bold)
+                                .foregroundColor(themeColor.opacity(1.0))
+                                .padding(.bottom, 1)
+                            Text(panel.subHeading)
+                                .font(.system(size: 8))
+                                .fontWeight(.regular)
+                                .foregroundColor(Color.gray)
+                                .padding(.bottom, 1)
                         }
                         Spacer()
                     }
-
+                    Spacer()
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+
             }
-            .padding(EdgeInsets(top: 10, leading: 15, bottom: 5, trailing: 15))
-            .background(background)
-            .cornerRadius(10)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
+        .padding(EdgeInsets(top: 10, leading: 15, bottom: 5, trailing: 15))
+        .background(background)
+        .cornerRadius(10)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+}
+
+class GaugePanel: DashboardPanel {
+    
+    var host1: NSHostingView<TimeSeriesContentView>!
+    var host2: NSHostingView<GaugeContentView>!
+    var subview: NSView? { return subviews.isEmpty ? nil : subviews[0] }
+    
+    override func mouseDown(with event: NSEvent) {
         
+        switchStyle()
+    }
+    
+    func switchStyle() {
+                    
+        if subview == host1 {
+            subview!.removeFromSuperview()
+            addSubview(host2)
+        } else if subview == host2 {
+            subview!.removeFromSuperview()
+            addSubview(host1)
+        } else {
+            addSubview(host1)
+        }
+        
+        if let subview = subview {
+            subview.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                subview.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                subview.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+                subview.topAnchor.constraint(equalTo: self.topAnchor),
+                subview.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            ])
+        }
+    }
+    
     required init?(coder aDecoder: NSCoder) {
-        
+
         super.init(coder: aDecoder)
 
-        host = NSHostingView(rootView: ContentView(model: model, panel: self))
-        self.addSubview(host)
+        host1 = NSHostingView(rootView: TimeSeriesContentView(model: model, panel: self))
+        host2 = NSHostingView(rootView: GaugeContentView(model: model, panel: self))
 
-        host.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            host.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            host.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            host.topAnchor.constraint(equalTo: self.topAnchor),
-            host.bottomAnchor.constraint(equalTo: self.bottomAnchor)
-        ])
+        switchStyle()
     }
 }
 
