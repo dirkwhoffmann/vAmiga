ab4b082f91a0635a60c232bb5b667cb19991de0c
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-01-08 15:38:57 +0100

    This is vAmiga 3.1.1

--- Emulator/config.h
+++ Emulator/config.h
@@ -16,7 +16,7 @@
 // Version number
 #define VER_MAJOR 3
 #define VER_MINOR 1
-#define VER_SUBMINOR 0
+#define VER_SUBMINOR 1
 #define VER_BETA 0
 
 // Snapshot version number
@@ -26,7 +26,7 @@
 #define SNP_BETA 0
 
 // Uncomment this setting in a release build
-// #define RELEASEBUILD
+#define RELEASEBUILD
 
 
 //
