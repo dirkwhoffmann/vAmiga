df7aa354e0f6df6db4904368778c000bf83cc1a1
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-02-04 09:26:02 +0100

    Added experimental quick-look code

---
+++ QuickLookPlugIn/PreviewProvider.swift
@@ -0,0 +1,60 @@
+//
+//  PreviewProvider.swift
+//  QuickLookPlugIn
+//
+//  Created by Dirk Hoffmann on 04.02.25.
+//  Copyright © 2025 Dirk Hoffmann. All rights reserved.
+//
+
+import Cocoa
+import Quartz
+
+class PreviewProvider: QLPreviewProvider, QLPreviewingController {
+    
+
+    /*
+     Use a QLPreviewProvider to provide data-based previews.
+     
+     To set up your extension as a data-based preview extension:
+
+     - Modify the extension's Info.plist by setting
+       <key>QLIsDataBasedPreview</key>
+       <true/>
+     
+     - Add the supported content types to QLSupportedContentTypes array in the extension's Info.plist.
+
+     - Change the NSExtensionPrincipalClass to this class.
+       e.g.
+       <key>NSExtensionPrincipalClass</key>
+       <string>$(PRODUCT_MODULE_NAME).PreviewProvider</string>
+     
+     - Implement providePreview(for:)
+     */
+    
+    func providePreview(for request: QLFilePreviewRequest) async throws -> QLPreviewReply {
+    
+        //You can create a QLPreviewReply in several ways, depending on the format of the data you want to return.
+        //To return Data of a supported content type:
+        
+        NSLog("dirkwhoffmann: loadView() ")
+        
+        let contentType = UTType.plainText // replace with your data type
+        // let contentType = UTType("de.dirkwhoffmann.retro.vamiga")!
+
+        let reply = QLPreviewReply.init(dataOfContentType: contentType, contentSize: CGSize.init(width: 200, height: 200)) { (replyToUpdate : QLPreviewReply) in
+
+            let data = Data("Holla, die Waldfee".utf8)
+            
+            //setting the stringEncoding for text and html data is optional and defaults to String.Encoding.utf8
+            replyToUpdate.stringEncoding = .utf8
+            
+            //initialize your data here
+            
+            return data
+        }
+              
+        // <string>$(PRODUCT_MODULE_NAME).PreviewViewController</string>
+        
+        return reply
+    }
+}
---
+++ QuickLookPlugIn/PreviewViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PreviewViewController.swift
+//  QuickLookPlugIn
+//
+//  Created by Dirk Hoffmann on 04.02.25.
+//  Copyright © 2025 Dirk Hoffmann. All rights reserved.
+//
+
+import Cocoa
+import Quartz
+
+class PreviewViewController: NSViewController, QLPreviewingController {
+
+    override var nibName: NSNib.Name? {
+        return NSNib.Name("PreviewViewController")
+    }
+
+    override func loadView() {
+        super.loadView()
+        // Do any additional setup after loading the view.
+        
+        NSLog("dirkwhoffmann: loadView()")
+    }
+
+    /*
+    func preparePreviewOfSearchableItem(identifier: String, queryString: String?) async throws {
+        // Implement this method and set QLSupportsSearchableItems to YES in the Info.plist of the extension if you support CoreSpotlight.
+
+        // Perform any setup necessary in order to prepare the view.
+        // Quick Look will display a loading spinner until this returns.
+    }
+    */
+
+    func preparePreviewOfFile(at url: URL) async throws {
+        // Add the supported content types to the QLSupportedContentTypes array in the Info.plist of the extension.
+
+        // Perform any setup necessary in order to prepare the view.
+
+        // Quick Look will display a loading spinner until this returns.
+        
+        NSLog("dirkwhoffmann: preparePreviewOfFile(/url))")
+    }
+
+}
