4d5feee3b00a26c39901256d0cd62a3bf460df9b
Dirk Hoffmann <dirk.hoffmann@me.com>
2025-09-24 11:54:28 +0200

    Work on onboarding

--- GUI/Metal/Layers/Tutorial.swift
+++ GUI/Metal/Layers/Tutorial.swift
@@ -1,48 +1,33 @@
+
 @MainActor
-class Tutorial: Layer {
+class TutorialLayerViewController: NSViewController {
 
-    var window: NSWindow { controller.window! }
-    var contentView: NSView { window.contentView! }
-    var storyboard: NSStoryboard { NSStoryboard(name: "Tutorial", bundle: nil) }
-
+    @IBOutlet weak var pageContainerView: NSView!
     var pageController: NSPageController!
+
     var objects: [String] = ["Step1", "Step2"]
 
-    override init(renderer: Renderer) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-        super.init(renderer: renderer)
-
-        pageController = storyboard.instantiateController(withIdentifier: "Tutorial") as? NSPageController
-
+        pageController = NSPageController()
         pageController.delegate = self
+        pageController.transitionStyle = .stackHistory
         pageController.arrangedObjects = objects
-        pageController.view.wantsLayer = true
-        pageController.view.layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
+
+        addChild(pageController)
+        pageContainerView.addSubview(pageController.view)
+        pageController.view.frame = pageContainerView.bounds
+        pageController.view.autoresizingMask = [.width, .height]
     }
-
-    override func alphaDidChange() {
-
-        pageController.view.alphaValue = CGFloat(alpha.current)
-
-        if alpha.current > 0 && pageController.view.superview == nil {
-            contentView.addSubview(pageController.view)
-            pageController.view.frame = contentView.bounds
-            pageController.view.autoresizingMask = [.width, .height]
-            pageController.navigateForward(nil) // show first page
-        }
-
-        if alpha.current == 0 && pageController.view.superview != nil {
-            pageController.view.removeFromSuperview()
-        }
-    }
-
 }
 
-extension Tutorial: NSPageControllerDelegate {
+extension TutorialLayerViewController: NSPageControllerDelegate {
 
     func pageController(_ pageController: NSPageController,
                         viewControllerForIdentifier identifier: String) -> NSViewController {
 
+        let storyboard = NSStoryboard(name: "Tutorial", bundle: nil)
         return storyboard.instantiateController(withIdentifier: identifier) as! NSViewController
     }
 
@@ -57,5 +42,79 @@ extension Tutorial: NSPageControllerDelegate {
 
             print("Finish...")
         }
+    }
+}
+
+@MainActor
+class Tutorial: Layer {
+
+    var window: NSWindow { controller.window! }
+    var contentView: NSView { window.contentView! }
+    var storyboard: NSStoryboard { NSStoryboard(name: "Tutorial", bundle: nil) }
+
+    var tutorialVC: TutorialLayerViewController!
+
+    // var pageController: NSPageController!
+
+
+    override init(renderer: Renderer) {
+
+        super.init(renderer: renderer)
+
+        tutorialVC = storyboard.instantiateController(withIdentifier: "TutorialLayerViewController") as? TutorialLayerViewController
+        tutorialVC.view.wantsLayer = true
+        tutorialVC.view.layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
+
+        // Add as child view controller if needed
+        /*
+        renderer.parent.addChild(tutorialVC)
+        renderer.parent.view.addSubview(tutorialVC.view)
+        tutorialVC.view.frame = renderer.parent.view.bounds
+        tutorialVC.view.autoresizingMask = [.width, .height]
+
+        // Now configure the page controller inside it
+        tutorialVC.pageController.delegate = tutorialVC  // or your layer
+        tutorialVC.pageController.arrangedObjects = objects
+         */
+
+        /*
+        pageController = storyboard.instantiateController(withIdentifier: "Tutorial") as? NSPageController
+
+        pageController.delegate = self
+        pageController.arrangedObjects = objects
+        pageController.view.wantsLayer = true
+        pageController.view.layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
+        */
+    }
+
+    override func alphaDidChange() {
+
+        tutorialVC.view.alphaValue = CGFloat(alpha.current)
+
+        if alpha.current > 0 && tutorialVC.view.superview == nil {
+            contentView.addSubview(tutorialVC.view)
+            tutorialVC.view.frame = contentView.bounds
+            tutorialVC.view.autoresizingMask = [.width, .height]
+            // tutorialVC.navigateForward(nil) // show first page
+        }
+
+        if alpha.current == 0 && tutorialVC.view.superview != nil {
+            tutorialVC.view.removeFromSuperview()
+        }
+
+        /*
+        pageController.view.alphaValue = CGFloat(alpha.current)
+
+        if alpha.current > 0 && pageController.view.superview == nil {
+            contentView.addSubview(pageController.view)
+            pageController.view.frame = contentView.bounds
+            pageController.view.autoresizingMask = [.width, .height]
+            pageController.navigateForward(nil) // show first page
+        }
+
+        if alpha.current == 0 && pageController.view.superview != nil {
+            pageController.view.removeFromSuperview()
+        }
+         */
     }
 }
