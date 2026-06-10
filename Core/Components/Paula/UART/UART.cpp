508df6e947a70720365be7bcb28114ae41403a35
Dirk Hoffmann <dirk.hoffmann@me.com>
2026-06-10 10:25:19 +0200

    Updated (outdated) license information in some files (#985)

--- Core/Components/Paula/UART/UART.cpp
+++ Core/Components/Paula/UART/UART.cpp
@@ -2,9 +2,9 @@
 // This file is part of vAmiga
 //
 // Copyright (C) Dirk W. Hoffmann. www.dirkwhoffmann.de
-// Licensed under the GNU General Public License v3
+// Licensed under the Mozilla Public License v2
 //
-// See https://www.gnu.org for license information
+// See https://mozilla.org/MPL/2.0 for license information
 // -----------------------------------------------------------------------------
 
 #include "config.h"
