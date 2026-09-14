8a92298bc4eb74f1c6599600fd860d347ff9ca39
Dirk Hoffmann <dirk.hoffmann@me.com>
2026-09-14 08:29:36 +0100

    Clearing M flag in execInterrupt

--- Core/Components/CPU/Moira/MoiraExceptions_cpp.h
+++ Core/Components/CPU/Moira/MoiraExceptions_cpp.h
@@ -569,12 +569,12 @@ Moira::execInterrupt(u8 level)
 
             queue.ird = getIrqVector(level);
 
-            // Both frame writers take the vector number, not the vector offset
             writeStackFrame0000<C>(status, reg.pc, queue.ird);
 
             if (reg.sr.m) {
 
-                writeStackFrame0001<C>(status, reg.pc, queue.ird);
+                setMasterMode(false);
+                writeStackFrame0001<C>(status | 0x2000, reg.pc, queue.ird);
             }
     }
 
